dbafb1858c01a39b2ed0878aaf2ac192b6d73129
DivijSasidhar <divijsasi@gmail.com>
2023-06-12 23:47:43 -0400

    working account system

--- client.py
+++ client.py
@@ -1,11 +1,59 @@
 import socket
+from hashlib import sha256
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65535  # The port used by the server
+# TODO: queue an action, have action finish at the end of the loop
+#   have a timeout system where if a response is not given in 10 seconds just retry
+# https://www.dunebook.com/creating-a-python-socket-server-with-multiple-clients/
+lc = ""
+
+
+def login():  # FIXME: universalize where the loop goes.
+    # FIXME: it should be client-side, probably in the main code so you can switch from create to login, vice-versa
+    while True:
+        username = input("Username: ")
+        password = input("Password: ")
+        s.sendall(bytes("LOGIN "
+                        "USERNAME " + username + " PASSWORD " + sha256(password.encode('utf-8')).hexdigest(), "utf-8"))
+        while True:
+            data = s.recv(1024)
+            if data == bytes("LOGINSUCCESS", 'utf-8'):
+                return "success"
+            if data == bytes("LOGINFAILURE", 'utf-8'):
+                print("Incorrect username or password.")
+                print(" ")
+                return
+
+
+def createaccount():
+    while True:
+        username = input("Select username: ")
+        password = input("Create password: ")
+        s.sendall(bytes("CREATE "
+                        "USERNAME " + username + " PASSWORD " + sha256(password.encode('utf-8')).hexdigest(), "utf-8"))
+        while True:
+            data = s.recv(1024)
+            if data == bytes("CREATIONSUCCESS", 'utf-8'):
+                return "success"
+            if data == bytes("CREATIONFAILURE", 'utf-8'):
+                print("Username taken.")
+                print("")
+                return
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    while not(lc == "L" or lc == "l" or lc == "C" or lc == "c"):
+        print("[L]ogin or [C]reate an account")
+        lc = input()
+        if lc == "L" or lc == "l":
+            while login() != "success":
+                pass
+        elif lc == "C" or lc == "c":
+            while createaccount() != "success":
+                pass
+        else:
+            print("Invalid input.")
 
-print(f"Received {data!r}")
+    print("Welcome to the server.")
--- server.py
+++ server.py
@@ -1,18 +1,87 @@
 import socket
+import time
+import json
+from _thread import *
 
 # https://realpython.com/python-sockets/
+# https://www.dunebook.com/creating-a-python-socket-server-with-multiple-clients/
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65535  # Port to listen on
+host = '127.0.0.1'
+port = 65535
+ThreadCount = 0  # number of connections
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+
+def start_server(host, port):
+    s = socket.socket()
+    try:
+        s.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    print(f'Your server (IP {host}) is listening (Port {port}).')
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)  # will receive at most 1024 bytes (if empty byte will break loop)
-            if not data:
-                break
-            conn.sendall(data)
+    while True:
+        accept_connections(s)
+
+
+def accept_connections(s):
+    Client, address = s.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(client_handler, (Client, ))
+
+
+def client_handler(connection):
+    print("Connection number " + str(ThreadCount) + ".")
+    while True:
+        data = connection.recv(1024).decode('utf-8')
+        split_data = data.split(" ", 1)
+        dataID = (split_data[0])  # each thing that the client sends will have an ID labeling what its trying to do
+        #  and a message accompanying it sometimes
+        try:
+            message = (split_data[1])
+        except IndexError:
+            message = ""  # if theres just an ID, set the message to nothing
+        if dataID == "LOGIN":
+            login(message, connection)
+        if dataID == "CREATE":
+            createaccount(message, connection)
+    connection.close()  # FIXME: make code reachable by letting user disconnect via input
+
+
+def login(message, connection):
+    username = (message.split("USERNAME", 1)[1].split("PASSWORD", 1))[0].strip()
+    password = (message.split("USERNAME", 1)[1].split("PASSWORD", 1))[1].strip()
+    f = open('savefile.json', 'r')
+    data = json.load(f)
+    for i in data['userDB']:
+        if i['username'] == username:
+            if i['password'] == password:
+                connection.send(bytes("LOGINSUCCESS", 'utf-8'))
+                f.close()
+    connection.send(bytes("LOGINFAILURE", 'utf-8'))
+    f.close()
+
+
+def createaccount(message, connection):
+    username = (message.split("USERNAME")[1].split("PASSWORD"))[0].strip()
+    password = (message.split("USERNAME")[1].split("PASSWORD"))[1].strip()
+    f = open('savefile.json', 'r+')
+    data = json.load(f)
+    for i in data['userDB']:
+        if username.lower() == i['username'].lower():
+            connection.send(bytes("CREATIONFAILURE", 'utf-8'))
+            f.close()
+            return
+    new_user_info = {"username": username,
+                     "password": password}
+    # my JSON format is a dictionary with a key 'userDB', its value being a list, and each element in the list
+    # is a dictionary of user info. that might be why i was struggling to parse and write to it.
+    data['userDB'].append(new_user_info)
+    new_data = json.dumps(data, indent=4)
+    f.close()
+    f = open('savefile.json', 'w')  # reopens the file to overwrite
+    f.write(new_data)
+    connection.send(bytes("CREATIONSUCCESS", 'utf-8'))
+    f.close()
+
+
+start_server(host, port)
